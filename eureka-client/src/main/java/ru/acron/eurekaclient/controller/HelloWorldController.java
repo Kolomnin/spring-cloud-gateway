0dd5001b1c80460d3fef9b7a2f7942cf78dcf0fa
Sergei <kolomninsp@gmail.com>
2024-02-10 15:35:41 +0300

    Добавлен Load Balancer

--- eureka-client/src/main/java/ru/acron/eurekaclient/controller/HelloWorldController.java
+++ eureka-client/src/main/java/ru/acron/eurekaclient/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package ru.acron.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
+//    @GetMapping("/world")
+//    public String hello(){
+//        return "Hello World!";
+//    }
+
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/world")
     public String hello(){
-        return "Hello World!";
+        return id;
     }
+
 }
 
